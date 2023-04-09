6cfa15569bbd0c4b21bf07e43e4b68ff896bd9ea
jakamum <90577895+jakamum@users.noreply.github.com>
2023-04-09 22:08:48 +1200

    Update Starburst.py

--- Turtle/Starburst.py
+++ Turtle/Starburst.py
@@ -3,6 +3,7 @@ from turtle import*
 
 delay(.9)
 
+# draw initial triangle
 def draw_star():
     forward(100)
     left(150)
@@ -12,20 +13,19 @@ def draw_star():
     left(200)
     left(350)
 
+# draw individual star  
 for i in range(9):
     draw_star()
 
 forward(150)
 
+# draw 3 more stars moving to a new position for eah star
 for i in range(3):
     for i in range(9):
         draw_star()
     left(90)
     forward(150)
 
-# draw_square()
-# left(90)
-# draw_square()
 if __name__=="main":
     exitonclick()
 
