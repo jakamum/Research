e60c9089e02a2722f197701d1168549f83a016b6
jakamum <90577895+jakamum@users.noreply.github.com>
2023-04-09 22:17:52 +1200

    Update shapesturtle.py

--- Turtle/shapesturtle.py
+++ Turtle/shapesturtle.py
@@ -1,26 +1,33 @@
 import turtle
 
+# code provided by Roman Mitch for us to obtain a practical understanding of classes and objects
+
+# create the abstract class 'Shape'
 class Shape:
     def __init__(self, sides, length, color):
         self.sides = sides
         self.length = length
         self.color = color
         self.t = turtle.Turtle()
-
+    
+    # draw method
     def draw(self):
         self.t.pencolor(self.color)
         for _ in range(self.sides):
             self.t.forward(self.length)
             self.t.right(360 / self.sides)
-
+            
+# create 'Square', child class of 'Shape'
 class Square(Shape):
     def __init__(self, length, color):
         super().__init__(4, length, color)
 
+# create 'Triangle', child class of 'Shape'
 class Triangle(Shape):
     def __init__(self, length, color):
         super().__init__(3, length, color)
-
+        
+# create 'Pentagon', child class of 'Shape'        
 class Pentagon(Shape):
     def __init__(self, length, color):
         super().__init__(5, length, color)
