500d4998041af4046a2b90fa523dfaca0f2ee138
jakamum <90577895+jakamum@users.noreply.github.com>
2023-04-09 22:54:23 +1200

    Update Let_There_Be_Free _Spiral.py

--- Spirals/Let_There_Be_Free _Spiral.py
+++ Spirals/Let_There_Be_Free _Spiral.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import math
 import time
 
-
+# commented area below for reference for myself
 # def spiral_text(canvas, text, x, y, radius, start_angle, font_size, font_type):
 #     # Set the initial angle
 #     angle = start_angle
